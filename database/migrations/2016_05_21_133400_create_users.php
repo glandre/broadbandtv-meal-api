e603b7307a1779b720ba0fd8c4ee020b9dcd48ab
Geraldo B. Landre <geraldo.landre@gmail.com>
2016-05-21 10:16:16 -0400

    User Authentication Started (#12)
    Signed-off-by: Geraldo B. Landre <geraldo.landre@gmail.com>

---
+++ database/migrations/2016_05_21_133400_create_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('api_token', 60)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
