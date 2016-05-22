943d5a1f95e6058d2537af7e3e09a438752ab057
magnosfire <rossini_pc@hotmail.com>
2016-05-22 13:00:14 -0300

    Adding URL in DB #14

--- database/seeds/ConfigurationsTableSeeder.php
+++ database/seeds/ConfigurationsTableSeeder.php
@@ -11,7 +11,10 @@ class ConfigurationsTableSeeder extends Seeder {
         DB::table('configurations')->delete();
  
         $configurations = array(
-            ['id' => 'USDA-API-KEY' , 'value' => 'BaKxZk2ziMCjeBGPJLlN8vw3VLmf2ypZbA6InZik']
+            ['id' => 'USDA-API-KEY' , 'value' => 'BaKxZk2ziMCjeBGPJLlN8vw3VLmf2ypZbA6InZik'],
+			['id' => 'USDA_REPORT_URL' , 'value' => 'http://api.nal.usda.gov/ndb/reports/'],
+			['id' => 'PREFERRED_FORMAT' , 'value' => 'json'],
+			['id' => 'USDA_SEARCH_URL' , 'value' => 'http://api.nal.usda.gov/ndb/search/'],
         );
  
         // Uncomment the below to run the seeder
