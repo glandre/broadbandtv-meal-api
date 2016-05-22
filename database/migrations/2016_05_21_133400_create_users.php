d47a95f5632c797d5ed73e7c918d2f4da2f4701d
Geraldo B. Landre <geraldo.landre@gmail.com>
2016-05-22 15:16:27 -0400

    User name unique (#12)
    Signed-off-by: Geraldo B. Landre <geraldo.landre@gmail.com>

--- database/migrations/2016_05_21_133400_create_users.php
+++ database/migrations/2016_05_21_133400_create_users.php
@@ -14,7 +14,7 @@ class CreateUsers extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('api_token', 60)->unique();
             $table->timestamps();
         });
